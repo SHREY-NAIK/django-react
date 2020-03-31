91e5820f8adefe657f07227799851abd477b41e3
Shrey Naik <shrey.naik@gmail.com>
2020-03-31 19:05:24 +0530

    django-react first commit

---
+++ leadmanager/frontend/src/components/leads/leads.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export class Leads extends Component {
+    render() {
+      return (
+        <div>
+        <h1>add Leads</h1>
+        </div>
+      )
+    }
+}
+export default Leads
